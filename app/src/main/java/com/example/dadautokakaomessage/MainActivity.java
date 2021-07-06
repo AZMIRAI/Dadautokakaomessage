5dd237f4bb054940e23e061d4301aa6285ff5922
KRHDY <1901322@g.yju.ac.kr>
2021-07-06 13:53:44 +0900

    앱

--- app/src/main/java/com/example/dadautokakaomessage/MainActivity.java
+++ app/src/main/java/com/example/dadautokakaomessage/MainActivity.java
@@ -1,14 +1,29 @@
 package com.example.dadautokakaomessage;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+
+import java.time.LocalDate;
 
 public class MainActivity extends AppCompatActivity {
 
+
+
+
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        Intent sendIntent = new Intent();
+        sendIntent.setAction(Intent.ACTION_SEND);
+        sendIntent.putExtra(Intent.EXTRA_TEXT, "This is my text to send.");
+        sendIntent.setType("text/plain");
+
+        Intent shareIntent = Intent.createChooser(sendIntent, null);
+        startActivity(shareIntent);
     }
 }
