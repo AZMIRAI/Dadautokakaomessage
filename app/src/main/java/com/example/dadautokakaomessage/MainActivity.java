9f3f400806035a8898d7a6084bc791a94000fd53
KRHDY <1901322@g.yju.ac.kr>
2021-07-06 19:33:46 +0900

    잠시수정

--- app/src/main/java/com/example/dadautokakaomessage/MainActivity.java
+++ app/src/main/java/com/example/dadautokakaomessage/MainActivity.java
@@ -20,7 +20,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         Intent sendIntent = new Intent();
         sendIntent.setAction(Intent.ACTION_SEND);
-        sendIntent.putExtra(Intent.EXTRA_TEXT, "This is my text to send.");
+        sendIntent.putExtra(Intent.EXTRA_TEXT, "" +
+                "0. 일시 :  2021. *.*.* * CBM\n" +
+                "1. Cell짱 : ***\n" +
+                "2. 업체명 : \n" +
+                "- **건설 (**,**,**)\n" +
+                "*명\n" +
+                "3. 고령자 출역인원 : \n" +
+                "- *명\n" +
+                "4. 출역 고령자 건강상태 : \n" +
+                "- *\n" +
+                "5. 작업내용 : \n" +
+                "- 각 동 * * *\n" +
+                "6. 위험성평가 위험작업 :\n" +
+                "- 작업중 개인보호구 착용 철저\n" +
+                "7. 사고사례전파내용 :\n" +
+                "- 전기 공도구 감전 주의\n");
         sendIntent.setType("text/plain");
 
         Intent shareIntent = Intent.createChooser(sendIntent, null);
